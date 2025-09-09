3a70b1a8783aa0c4ab323057cc4f422e79071915
aryansenpaii <aryansenpai256@gmail.com>
2025-09-09 07:21:59 +0530

    My first commit to this project🚀

---
+++ src/notepad/Notepad.java
@@ -0,0 +1,22 @@
+package notepad;
+import javax.swing.*;
+import java.awt.*;
+public class Notepad extends JFrame {
+
+    Notepad(){
+        setTitle("Notepad");
+        ImageIcon notepadIcon = new ImageIcon(ClassLoader.getSystemResource("notepad/icons/notepad.png"));
+        Image icon= notepadIcon.getImage();
+        setIconImage(icon);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        
+        
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Notepad();
+    }
+    
+}
