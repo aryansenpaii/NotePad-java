b1d6d3f7d4af8ad27a367c2adb68dfdb43ce621c
aryansenpaii <aryansenpai256@gmail.com>
2025-09-10 20:39:59 +0530

    Open and New option works now!

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -2,8 +2,10 @@ package notepad;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Notepad extends JFrame {
-
+import javax.swing.filechooser.*;
+import java.io.*;
+public class Notepad extends JFrame implements ActionListener{
+    JTextArea textArea;
     Notepad(){
         setTitle("Notepad");//set the title of the notepad app
         //creating and setting the application icon
@@ -19,12 +21,15 @@ public class Notepad extends JFrame {
         file.setFont(new Font("Aerial",Font.PLAIN, 14)); //setting font size
         //File MENU Options
         JMenuItem newDoc= new JMenuItem("New");
+        newDoc.addActionListener(this);
         newDoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK)); //setting mnemonics
         
         JMenuItem open= new JMenuItem("Open");
+        open.addActionListener(this);
         open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK)); //setting mnemonics
         
         JMenuItem save= new JMenuItem("Save");
+        save.addActionListener(this);
         save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK)); //setting mnemonics
         
         JMenuItem print= new JMenuItem("Print");
@@ -77,11 +82,50 @@ public class Notepad extends JFrame {
         menuBar.add(edit);
         menuBar.add(helpMenu);
         
-        
         setJMenuBar(menuBar);
+        
+        //CREATE TEXT AREA
+        textArea= new JTextArea();
+        textArea.setFont(new Font("SAN_SERIF",Font.PLAIN,18));
+        textArea.setLineWrap(true);//SETTING WORD-WRAP
+        textArea.setWrapStyleWord(true);
+        
+        JScrollPane scrollPane= new JScrollPane(textArea);//ENABLE SCROLLBAR
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        add(scrollPane);
+        
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         
         setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        //RESET THE TEXT EDITOR TO MAKE A NEW FILE
+        if(ae.getActionCommand().equals("New")){
+            textArea.setText("");
+        }
+        //OPEN FILES
+        else if(ae.getActionCommand().equals("Open")){
+            JFileChooser chooser = new JFileChooser();
+            //TO ONLY ACCEPT TXT FILES
+            chooser.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter restrict = new FileNameExtensionFilter("Only .txt files","txt");
+            chooser.addChoosableFileFilter(restrict);
+            //To show open dialog box based on the parent class
+            int action = chooser.showOpenDialog(this);
+            //what if we didn't choose any file and just close,(go back to parent class)
+            if(action!= JFileChooser.APPROVE_OPTION){
+                return;
+            }
+            //File Handling to read the file
+            File file = chooser.getSelectedFile();
+            try {
+                BufferedReader reader= new BufferedReader(new FileReader(file));
+                textArea.read(reader,null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     public static void main(String[] args) {
         // TODO code application logic here
